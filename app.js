784fe3df681d156b44eb9e740617f0488fe24f49
Muhammad Rizwan <muhammad.rizwan76146@gmail.com>
2022-12-19 22:04:57 +0700

    admin lte installation

--- app.js
+++ app.js
@@ -1,39 +1,44 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
 
-var categoryRouter = require('./app/category/router');
+var categoryRouter = require("./app/category/router");
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+app.engine('ejs', require('ejs').__express);
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(
+  "/adminlte",
+  express.static(path.join(__dirname, "/node_modules/admin-lte"))
+);
 
-app.use('/', categoryRouter);
+app.use("/", categoryRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
--- db/index.js
+++ db/index.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const { urlDb } = require("../config");
 
+mongoose.set('strictQuery', false);
 
 mongoose.connect(urlDb,{
     useNewUrlParser: true,
