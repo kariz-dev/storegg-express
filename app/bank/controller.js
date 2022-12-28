45f67163c69538f2d3ebcc6a8dbd378513093a48
Muhammad Rizwan <muhammad.rizwan76146@gmail.com>
2022-12-29 02:34:22 +0700

    CRUD Read bank page

---
+++ app/bank/controller.js
@@ -0,0 +1,51 @@
+const Bank = require("./model");
+
+module.exports = {
+  index: async (req, res) => {
+    try {
+      const alertMessage = req.flash("alertMessage");
+      const alertStatus = req.flash("alertStatus");
+
+      const alert = { message: alertMessage, status: alertStatus };
+      const bank = await Bank.find();
+
+      res.render("admin/bank/view_bank", {
+        title: "Bank | Store GG",
+        bank,
+        alert,
+      });
+    } catch (err) {
+      req.flash("alertMessage", `${err.message}`);
+      req.flash("alertStatus", "danger");
+      res.redirect("/bank");
+    }
+  },
+
+  viewCreate: async (req, res) => {
+    try {
+      res.render("admin/bank/create");
+    } catch (err) {
+      req.flash("alertMessage", `${err.message}`);
+      req.flash("alertStatus", "danger");
+      res.redirect("/bank");
+    }
+  },
+
+  actionCreate: async (req, res) => {
+    try {
+      const { name, nameBank, noRekening } = req.body;
+
+      let bank = await Bank({ name, nameBank, noRekening });
+      await bank.save();
+
+      req.flash("alertMessage", "Berhasil tambah bank");
+      req.flash("alertStatus", "success");
+
+      res.redirect("/bank");
+    } catch (err) {
+      req.flash("alertMessage", `${err.message}`);
+      req.flash("alertStatus", "danger");
+      res.redirect("/bank");
+    }
+  },
+};
