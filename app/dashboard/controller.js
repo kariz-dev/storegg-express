aa8d18f9eb73f572bc4add314bd5dcb8a4de49b3
Muhammad Rizwan <muhammad.rizwan76146@gmail.com>
2023-01-03 03:45:49 +0700

    dashboard page

--- app/dashboard/controller.js
+++ app/dashboard/controller.js
@@ -1,9 +1,25 @@
+const Transaction = require("../transaction/model");
+const Voucher = require("../voucher/model");
+const Player = require("../player/model");
+const Category = require("../category/model");
+
 module.exports = {
   index: async (req, res) => {
     try {
-      res.render("index", {
+      const transaction = await Transaction.countDocuments();
+      const voucher = await Voucher.countDocuments();
+      const player = await Player.countDocuments();
+      const category = await Category.countDocuments();
+
+      res.render("admin/dashboard/view_dashboard", {
         title: "Dashboard | Store GG",
         name: req.session.user.name,
+        count: {
+          transaction,
+          voucher,
+          player,
+          category,
+        },
       });
     } catch (err) {
       console.log(err);
---
+++ app/player/model.js
@@ -0,0 +1,53 @@
+const mongoose = require("mongoose");
+
+let playerSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      require: [true, "nama harus diisi"],
+      maxlength: [225, "panjang nama harus antara 3 - 225 karakter"],
+      minlength: [3, "panjang nama harus antara 3 - 225 karakter"],
+    },
+    userName: {
+      type: String,
+      require: [true, "nama harus diisi"],
+      maxlength: [225, "panjang username harus antara 3 - 225 karakter"],
+      minlength: [3, "panjang username harus antara 3 - 225 karakter"],
+    },
+    email: {
+      type: String,
+      require: [true, "email harus diisi"],
+    },
+    password: {
+      type: String,
+      require: [true, "kata sandi harus diisi"],
+      maxlength: [225, "panjang password harus antara 6 - 225 karakter"],
+      minlength: [6, "panjang password harus antara 6 - 225 karakter"],
+    },
+    phoneNumber: {
+      type: String,
+      require: [true, "nomor telepon harus diisi"],
+      maxlength: [14, "panjang nomor telepon harus antara 9 - 225 karakter"],
+      minlength: [8, "panjang nomor telepon harus antara 9 - 225 karakter"],
+    },
+    role: {
+      type: String,
+      enum: ["admin", "user"],
+      default: "user",
+    },
+    avatar: { type: String },
+    fileName: { type: String },
+    status: {
+      type: String,
+      enum: ["Y", "N"],
+      default: "Y",
+    },
+    favorite: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Category",
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Player", playerSchema);
